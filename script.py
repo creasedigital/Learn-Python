a59707dce9ae21ea4ce13d11021cf4a424576400
creasedigital <ojorechris@gmail.com>
2022-02-10 20:14:43 +0100

    data types

--- script.py
+++ script.py
@@ -1,6 +1,28 @@
+# This is a single line comment
+
+# Below is a multi-line comment
 '''print('Hello, world!')
 print(1 + 2)
 trying out python
 '''
-"print('Hello\n Python')"
-print('Python is \n awesome')
+# Below is a single line comment also
+# "print('Hello\n Python')"
+
+# Here is how we print to our console
+# print('Python is \n awesome')
+
+# Data Types type() command is used to check a data type
+'''
+The Computer needs to know
+the kind of data you want it
+to process
+'''
+# strings - are represented with any character inside single or double quotes e.g. 'John'
+# int - also known as integers are represented with whole numbers value e.g. 546892
+# float -  are represented with decimal numbers value e.g. 63.52
+# bool - booleans are represented with a True or False value
+
+print(type('John'))
+print(type(546892))
+print(type(63.52))
+print(type(False))
