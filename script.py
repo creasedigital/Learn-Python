58a140edf4499cbc8fa54c5f0b7dc576b7d9d41d
creasedigital <ojorechris@gmail.com>
2022-02-12 15:05:11 +0100

    few lines added

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+# -------------------------------------COMMENTS
 # This is a single line comment
 
 # Below is a multi-line comment
@@ -11,6 +12,7 @@ trying out python
 # Here is how we print to our console
 # print('Python is \n awesome')
 
+# -------------------------------------DATA TYPES
 # Data Types type() command is used to check a data type
 '''
 The Computer needs to know
@@ -22,7 +24,23 @@ to process
 # float -  are represented with decimal numbers value e.g. 63.52
 # bool - booleans are represented with a True or False value
 
+
 print(type('John'))
 print(type(546892))
 print(type(63.52))
 print(type(False))
+
+# ----------------------------------------VARIABLES
+# Naming conventions used in python includes:
+'''The use of underscore to connect values rather than space'''
+# variables are use to store values to make them reusable
+
+user_age = 50
+user_location = 'Abuja'
+user_height = 186
+
+# variable reassignment
+user_age = 30
+
+# commas are used to declare multiple values in a function
+print(user_age, user_location, user_height)
