c2225e686c65422c8f7331eee1e53ef40a385ccf
creasedigital <ojorechris@gmail.com>
2022-02-25 05:32:36 +0100

    mad libs game added

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+# print('Roses are {color}')
+# print('{plural noun} are blue')
+# print('I love {celebrity}')
+
+color = input('Enter a color: ')
+plural_noun = input('Enter a plural noun: ')
+celebrity = input('Enter a celebrity: ')
+
+print('Roses are ' + color)
+print(plural_noun + ' are blue')
+print('I love' + celebrity)
