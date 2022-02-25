a03780cf0a90213d5e4b2738039a7212b24abee6
creasedigital <ojorechris@gmail.com>
2022-02-25 05:58:30 +0100

    list basics completed

---
+++ list.py
@@ -0,0 +1,10 @@
+friends = ['Kelvin', 'Karen', 'Jim', 'Champion', 'Drury']
+
+print(friends[2])
+
+print('My commentators are: Jon ' +
+      friends[3] + ' and ' + friends[2] + ' Beglin')
+
+print(friends[1:])
+print(friends[1:2])
+print(friends[1:-1])
