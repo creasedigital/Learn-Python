8eed7bcbf49243db9109400aa3cb313cca868d1b
creasedigital <ojorechris@gmail.com>
2022-02-25 06:22:40 +0100

    list functions were added

--- list.py
+++ list.py
@@ -1,10 +1,35 @@
-friends = ['Kelvin', 'Karen', 'Jim', 'Champion', 'Drury']
+friends = ['Kelvin', 'Jim', 'Karen', 'Jim', 'Champion', 'Jim', 'Drury']
+lucky_numbers = [8, 4, 42, 7, 5]
 
-print(friends[2])
+# friends.extend(lucky_numbers)
+friends.append('Ronaldo')  # adds last element to the list
 
-print('My commentators are: Jon ' +
-      friends[3] + ' and ' + friends[2] + ' Beglin')
+# adds quoted element to the specified list position
+friends.insert(0, 'Shelvey')
 
-print(friends[1:])
-print(friends[1:2])
-print(friends[1:-1])
+friends.pop()  # removes last element from the list
+
+print(friends)
+
+print(friends.count('Jim'))  # counts the number of that item found in the list
+
+friends.reverse()  # reverse the list
+
+friends.sort()  # sort the list alphabetically
+
+
+print(friends.index('Drury'))
+
+friends2 = friends.copy()
+
+print(friends)
+print(friends2)
+
+# print(friends[2])
+
+# print('My commentators are: Jon ' +
+#       friends[3] + ' and ' + friends[2] + ' Beglin')
+
+# print(friends[1:])
+# print(friends[1:2])
+# print(friends[1:-1])
