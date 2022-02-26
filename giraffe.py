fd5ce948b80855550d02a131ab2410bea8ad9fbb
creasedigital <ojorechris@gmail.com>
2022-02-26 17:00:20 +0100

    comparism operators

--- giraffe.py
+++ giraffe.py
@@ -22,24 +22,39 @@
 
 # if statement (conditionals) helps make our program smarter
 
-is_male = False
-is_tall = True
+# is_male = False
+# is_tall = True
 
-if is_male or is_tall:
-    print("Please use the gents, Mr. tola?")
-else:
-    print("Please use the ladies, Ms. Petty")
+# if is_male or is_tall:
+#     print("Please use the gents, Mr. tola?")
+# else:
+#     print("Please use the ladies, Ms. Petty")
 
-if is_male and is_tall:
-    print("Hi Boss")
-else:
-    print("Hello Madam!")
+# if is_male and is_tall:
+#     print("Hi Boss")
+# else:
+#     print("Hello Madam!")
 
-if is_male and is_tall:
-    print("Hi Boss")
-elif is_male and not(is_tall):
-    print("Hi Little Man")
-elif not(is_male) and is_tall:
-    print("Hi Cutie")
-else:
-    print("Hello Madam!")
+# if is_male and is_tall:
+#     print("Hi Boss")
+# elif is_male and not(is_tall):
+#     print("Hi Little Man")
+# elif not(is_male) and is_tall:
+#     print("Hi Cutie")
+# else:
+#     print("Hello Madam!")
+
+
+# comparism operators
+# comparism involving other data typesaside booleans only
+
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+
+print(max_num(69, 524, 302))
