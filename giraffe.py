12bf5de5843c428a27dc5a11532d8fe9809d40b2
creasedigital <ojorechris@gmail.com>
2022-02-26 16:56:30 +0100

    return statements

--- giraffe.py
+++ giraffe.py
@@ -1,11 +1,21 @@
 # functions
 # -- A collction of codes that performs specific task--
 
-user = input('Type your name: ')
+# user = input('Type your name: ')
 
 
-def say_hi():
-    print("hello " + user)
+# def say_hi():
+#     print("hello " + user)
 
 
-say_hi()
+# say_hi()
+
+
+# return statement
+def cube(num):
+    return num**3
+
+
+print(cube(3))
+print(cube(4))
+print(cube(6))
