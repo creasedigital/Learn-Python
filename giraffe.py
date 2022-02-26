a474718eb2ba916bdf77d9bb1da71da23e82e8db
creasedigital <ojorechris@gmail.com>
2022-02-26 17:02:08 +0100

    Calculator App

--- giraffe.py
+++ giraffe.py
@@ -48,13 +48,31 @@
 # comparism operators
 # comparism involving other data typesaside booleans only
 
-def max_num(num1, num2, num3):
-    if num1 >= num2 and num1 >= num3:
-        return num1
-    elif num2 >= num1 and num2 >= num3:
-        return num2
-    else:
-        return num3
+# def max_num(num1, num2, num3):
+#     if num1 >= num2 and num1 >= num3:
+#         return num1
+#     elif num2 >= num1 and num2 >= num3:
+#         return num2
+#     else:
+#         return num3
 
 
-print(max_num(69, 524, 302))
+# print(max_num(69, 524, 302))
+
+
+# Calculator App
+
+num1 = float(input('Enter the first number: '))
+operator = input('Enter operator: ')
+num2 = float(input('Enter the second number: '))
+
+if operator == '+':
+    print(num1 + num2)
+elif operator == '-':
+    print(num1 - num2)
+elif operator == '/':
+    print(num1 / num2)
+elif operator == '*':
+    print(num1 * num2)
+else:
+    print('invalid operator!')
