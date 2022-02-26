9af54a6e356ad06c5371dce912535b99b198298b
creasedigital <ojorechris@gmail.com>
2022-02-26 16:58:24 +0100

    conditionals

--- giraffe.py
+++ giraffe.py
@@ -12,10 +12,34 @@
 
 
 # return statement
-def cube(num):
-    return num**3
+# def cube(num):
+#     return num**3
 
 
-print(cube(3))
-print(cube(4))
-print(cube(6))
+# print(cube(3))
+# print(cube(4))
+# print(cube(6))
+
+# if statement (conditionals) helps make our program smarter
+
+is_male = False
+is_tall = True
+
+if is_male or is_tall:
+    print("Please use the gents, Mr. tola?")
+else:
+    print("Please use the ladies, Ms. Petty")
+
+if is_male and is_tall:
+    print("Hi Boss")
+else:
+    print("Hello Madam!")
+
+if is_male and is_tall:
+    print("Hi Boss")
+elif is_male and not(is_tall):
+    print("Hi Little Man")
+elif not(is_male) and is_tall:
+    print("Hi Cutie")
+else:
+    print("Hello Madam!")
