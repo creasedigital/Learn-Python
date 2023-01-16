62eae4834f6290202176a6e27f1c2dfc51b548c7
Ojore Chris <ojorechris@gmail.com>
2023-01-16 13:42:26 +0100

    logic

---
+++ logic.py
@@ -0,0 +1,11 @@
+animal = input('Enter the name of your favourite pet\n')
+
+
+if animal == 'cat':
+    print('meow, I love you')
+elif animal == 'dog':
+    print("woof! woof!!, you're my hero")
+elif animal:
+    print(f'wow!!! the {animal} is my favourite pet')
+else:
+    print('Please enter the name of your favourite pet')
