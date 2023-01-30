bb2d174b069e05d90e009ffe37ca84008617ad38
Ojore Chris <ojorechris@gmail.com>
2023-01-30 02:33:04 +0100

    try block repositioned

--- repeat.py
+++ repeat.py
@@ -1,6 +1,6 @@
-nums = int(input('How many times do I have to tell you?\n'))
-
 try:
+    nums = int(input('How many times do I have to tell you?\n'))
+
     if type(nums) == int:
         for num in range(nums):
             print('CLEAN UP YOUR ROOM!')
