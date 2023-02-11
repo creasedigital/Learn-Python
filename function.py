d0f3703a3c135f92e9c477bfe099ce2b418ade8e
Ojore Chris <ojorechris@gmail.com>
2023-02-11 10:47:41 +0100

    function.py

---
+++ function.py
@@ -0,0 +1,8 @@
+def sing_happy_birthday(name="friend"):
+    print("Happy birthday to you")
+    print("Happy birthday to you")
+    print(f"Happy birthday to you, {name}")
+    print("Happy birthday to you")
+
+
+print("Michael")
