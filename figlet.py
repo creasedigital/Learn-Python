9227e5a82ecee7a619f10ca1b22f0a8ba0108f4b
Ojore Chris <ojorechris@gmail.com>
2023-02-15 17:20:39 +0100

    refactored

--- figlet.py
+++ figlet.py
@@ -1,10 +1,12 @@
-import pyfiglet
+from pyfiglet import figlet_format
 from termcolor import colored
 
 text = input("Enter the text to display: \n")
 text_color = input("Pick your fav color: \n")
 
-col = colored(pyfiglet.figlet_format(text), color=text_color,
+ascii_art = figlet_format(text)
+
+col = colored(ascii_art, color=text_color,
               on_color="on_magenta", attrs={'blink': 3})
 
 print(col)
