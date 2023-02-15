ef5ab85b746b832a1bd141938fd027799b6d40c2
Ojore Chris <ojorechris@gmail.com>
2023-02-15 16:41:25 +0100

    term color

--- colors.py
+++ colors.py
@@ -1,3 +1,8 @@
-import termcolor
+from termcolor import colored
+text = colored("Helloooooooow!!!", color="yellow",
+               on_color="on_magenta", attrs=["blink"])
+print(text)
 
-print(dir(termcolor))
+# print(dir(termcolor))
+
+# help(termcolor)
