50fd276a82b3d188b660e5fb06a8317ace9bfc2f
Ojore Chris <ojorechris@gmail.com>
2023-02-16 18:29:20 +0100

    search-done

--- dadjokes.py
+++ dadjokes.py
@@ -10,21 +10,22 @@ res = requests.get(
     url,
     headers={
         "Accept": "application/json",
-        "params": search_term
+    },
+    params={
+        "term": search_term
     }
     
-)
+).json()
 
-results = res.json()['results']
-
-random_joke_num = len(results)
-random_joke = choice(results)['joke']
+result = res['results']
+random_joke_num = len(result)
+random_joke = choice(result)['joke']
 
 if random_joke_num == 0:
 
     print(f"I don't have any joke about {search_term}. Please try something else \n{random_joke}")
 elif random_joke_num == 1:
     print(f"I have {random_joke_num} joke about {search_term}. Here it is: \n{random_joke}")
-    
-print(f"I have {random_joke_num} jokes about {search_term}. Here's one: \n{random_joke}")
+else:
+    print(f"I have {random_joke_num} jokes about {search_term}. Here's one: \n{random_joke}")
 
