75eddecba7225df2b233bf4643172c93c337ad69
Ojore Chris <ojorechris@gmail.com>
2023-02-16 16:05:41 +0100

    search term and randomization

--- dadjokes.py
+++ dadjokes.py
@@ -1,9 +1,30 @@
 import requests
-from random import shuffle
+from random import choice
 
 url="https://icanhazdadjoke.com/search"
+search_term = input("Let me tell you a funny joke!!! Give me a topic: ")
+
+
 
 res = requests.get(
     url,
+    headers={
+        "Accept": "application/json",
+        "params": search_term
+    }
     
 )
+
+results = res.json()['results']
+
+random_joke_num = len(results)
+random_joke = choice(results)['joke']
+
+if random_joke_num == 0:
+
+    print(f"I don't have any joke about {search_term}. Please try something else \n{random_joke}")
+elif random_joke_num == 1:
+    print(f"I have {random_joke_num} joke about {search_term}. Here it is: \n{random_joke}")
+    
+print(f"I have {random_joke_num} jokes about {search_term}. Here's one: \n{random_joke}")
+
