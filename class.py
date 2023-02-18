a1be798ec37c1a943fc5b1e1c7006edea815f700
Ojore Chris <ojorechris@gmail.com>
2023-02-18 07:58:48 +0100

    creating class instances

--- class.py
+++ class.py
@@ -1,2 +1,25 @@
 class User:
-    pass
+    def __init__(self, first, last, age):
+        self.first = first
+        self.last = last
+        self.age = age
+
+    def fullname(self):
+        return f"{self.first} {self.last}"
+
+    def initials(self):
+        return f"{self.first[0]}.{self.last[0]}."
+    
+    def likes(self, like):
+        return f"{self.first} likes {like}"
+
+        
+    
+        
+
+user1 = User("Punk", "Manewe", 31)
+print(user1.fullname())
+print(user1.likes("eating"))
+
+
+
