8c2a27a3c2d3293d2c71bdbe7bac1efc87c9e693
Ojore Chris <ojorechris@gmail.com>
2023-02-23 17:46:21 +0100

    generator

--- gen_nums.py
+++ gen_nums.py
@@ -1,4 +1,4 @@
-def gen_nums():
+""" def gen_nums():
     max_num = 100
     nums = (1, 2, 3, 4)
     i = 0
@@ -9,4 +9,21 @@ def gen_nums():
         i += 1
     return results
     
-print(gen_nums())
+print(gen_nums()) """
+
+# using generators for optimization
+def gen_nums():
+    nums = (1,2,3,4)
+    i=0
+    while True:
+        if i >= len(nums): i = 0
+        yield nums[i]
+        i += 1
+            
+gen = gen_nums()
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+
