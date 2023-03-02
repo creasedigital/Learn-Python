d7fa2f58103defa30ce91bdb9a0630b08f9b5aba
Ojore Chris <ojorechris@gmail.com>
2023-03-02 17:22:43 +0100

    html file to test web scraping

---
+++ basichtml.py
@@ -0,0 +1,21 @@
+html = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>First HTML Page</title>
+</head>
+<body>
+    <div id="first">
+        <h3 data-example="yes">hi</h3>
+        <p>more text</p>
+    </div>
+    <ol>
+    <li>This list item is special.</li>
+    <li>This list item is also special.</li>
+    <li>This list item is not special.</li>
+    </ol>
+    <div>bye</div>
+</body>
+</html>
+"""
